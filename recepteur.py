50f974eac668b5a17c50c012647e1ef9f6dfc3b0
Mixinium <39061106+Mixinium@users.noreply.github.com>
2021-12-08 19:07:52 +0100

    Affinement

--- recepteur.py
+++ recepteur.py
@@ -1,6 +1,7 @@
 from microbit import *
 import radio
-uart.init(115200)
+
+uart.init(baudrate=115200)
 msg_bytes = uart.read()
 baudrate = 115200
 msg_str = str(msg_bytes)
@@ -8,11 +9,38 @@ microbit_uart_timeout_char = 13000 / baudrate + 1
 radio.config(length=32, queue=3, channel=18, power=0, address=0x75626974, group=0, data_rate=radio.RATE_1MBIT)
 radio.on()
 test = 0
+mouv=0
+display.scroll('Pret')
 
 while True:
     msg_bytes = uart.read()
     msg_str = str(msg_bytes)
     incomming = radio.receive()
+
+
+    if incomming == 'a':
+        display.scroll('Recu')
+        msg_bytes = uart.read()
+        if msg_bytes == b'mouvement':
+
+            print('1')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     if incomming == 'a':
         test+=1
         display.show(test)
@@ -21,7 +49,3 @@ while True:
         test-=1
         display.show(test)
         print(msg_str)
-
-
-
-
--- tee.py
+++
@@ -1,9 +0,0 @@
-from threading import Timer
-
-def timeout():
-    print("Game over")
-
-t = Timer(1, timeout)
-t.start()
-
-t.join()
